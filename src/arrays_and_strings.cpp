92f8da759e3520e35981977f99f9b3b5a8066c30
Noa Vukasovic <noavukasovic@gmail.com>
2022-11-13 17:25:12 +0100

    Add Problem: unique_characters (3 different implementations) & Test in main

--- src/arrays_and_strings.cpp
+++ src/arrays_and_strings.cpp
@@ -1,1 +1,88 @@
+#include <unordered_set>
 #include "arrays_and_strings.h"
+
+/*
+	If it's not only lowercase English letters then we could use a
+	bit-vector only if it's ASCII(not extended ASCII(256))
+
+	Max single dataType is "long double" which is 16B on 64-bit architecture
+	16B = 128 bits
+*/
+
+/* ======================================================================== */
+/* === Assumption: String is consisted of only lowecase English letters === */
+/* ======================================================================== */
+
+/* Time  Complexity O(1) if we know it's only ASCII 128 or under */
+/* Space Complexity O(1) if we know it's only ASCII 128 or under */
+bool
+unique_characters_1(std::string s)
+{
+	// Assume that we use only use lowercase English letters
+	if (s.length() > 27)
+		return false; // Then it's sure there are repeating characters
+	
+	int bit_vector = 0;
+	for (unsigned i = 0; i < s.length(); i++)
+	{
+		int char_value = s[i] - 'a';
+
+		if (bit_vector & (1 << char_value))
+			return false;
+
+		bit_vector |= (1 << char_value);
+	}
+
+	return true;
+}
+
+
+/* ========================================================================= */
+/* === Assumption: String is consisted of all possible characters(UTF-8) === */
+/* ========================================================================= */
+
+/* Time  Complexity O(n) */
+/*
+	Space Complexity O(min(n, u))
+	
+	where 'u' is a number of unique characters in the string. If the string is
+	indeed consisted of unique characters then 'u' will be equal to 'n'
+*/
+bool
+unique_characters_2(std::string s)
+{
+	std::unordered_set<char> uset;
+
+	for (auto x: s)
+	{
+		if (uset.find(x) == uset.end())
+			uset.insert(x);
+		else
+			return false;
+	}
+
+	return true;
+}
+
+
+/* ========================================================================= */
+/* === Constraint: We aren't allowed to use an additional data structure === */
+/* ========================================================================= */
+
+/* Time  Complexity O(n^2) */
+/* Space Complexity O(1)   */
+bool
+unique_characters_3(std::string s)
+{
+	int n = s.length();
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j >= 0; j--)
+		{
+			if (i != j && s[i] == s[j])
+				return false;
+		}
+	}
+
+	return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,28 @@
 int
 main()
 {
-	std::cout << "\n\tInitial structure\n\n";
+	/* =============== */
+	/* === TESTING === */
+	/* =============== */
+
+	/* Unique Characters */
+	std::cout << "\n\t=========================\n";
+	std::cout << "\t=== UNIQUE CHARACTERS ===\n";
+	std::cout << "\t=========================\n\n";
+
+	/* Example 1 */
+	std::string s = "algorithm";
+
+	/* Example 2 */
+	// std::string s = "manacher";
+
+	if (unique_characters_1(s))
+	// if (unique_characters_2(s))
+	// if (unique_characters_3(s))
+		std::cout << "\n\tThe String: \"" << s << "\" is INDEED consisted of unique characters!\n\n";
+	else
+		std::cout << "\n\tThe String: \"" << s << "\" is NOT consisted of unique characters!\n\n";
+
 
 	return 0;
 }
